00dec3d91b212ad74474ca8025f22bd3a1829e24
rishugupta1 <rishabhitorya@gmail.com>
2026-05-14 17:33:56 +0530

    fixed database error

--- load_database.py
+++ load_database.py
@@ -10,14 +10,25 @@ load_dotenv()
 # GOOGLE SHEET API URL
 GOOGLE_SHEET_API = os.getenv("GOOGLE_SHEET_API")
 
-# FETCH DATA
-response = requests.get(GOOGLE_SHEET_API)
+try:
 
-# CONVERT JSON
-data = response.json()
+    # FETCH DATA
+    response = requests.get(GOOGLE_SHEET_API)
 
-# CREATE DATAFRAME
-products_df = pd.DataFrame(data)
+    print("STATUS CODE:", response.status_code)
+    print("RAW RESPONSE:", response.text)
 
-# PRINT DATA
-print(products_df)
+    # CONVERT JSON
+    data = response.json()
+
+    # CREATE DATAFRAME
+    products_df = pd.DataFrame(data)
+
+    print(products_df)
+
+except Exception as e:
+
+    print("DATABASE LOAD ERROR:", e)
+
+    # EMPTY DATAFRAME
+    products_df = pd.DataFrame()
